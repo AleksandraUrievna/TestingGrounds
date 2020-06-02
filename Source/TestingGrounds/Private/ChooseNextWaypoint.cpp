b37e18f68df1ab5392bc2f2bc818a1b1a6eee754
AleksandraUrievna <kylie628659@gmail.com>
2020-06-02 16:56:14 +0300

    18. Reading Blackboard Data in C++
    Expose a blackboard key selector in C++
    #include “BehaviorTree/BlackboardComponent.h”
    OwnerComp.GetBlackboardComponent()
    Use GetValueAsInt() etc to get the value
    You can now access blackboard data in C++.

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,8 +2,11 @@
 
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	return EBTNodeResult::Succeeded;
 }
