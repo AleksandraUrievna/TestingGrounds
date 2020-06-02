f0bacf2a993d0b3c3f94001f4d0ecf20c7ea51d2
AleksandraUrievna <kylie628659@gmail.com>
2020-06-02 20:47:15 +0300

    20. Converting Blueprint to C++
    OwnerComp.GetAIOwner() to get AI Controller
    If you’re casting, you’re probably coupling
    Use .Num() to get number of elements in TArray
    Use TODO for reminders, to help you FOCUS
    FOCUS = Follow One Course Until Successful.

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -3,10 +3,27 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
+
+
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// Get Patrol Points
+	auto AIController=OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointersCPP;
+
+	//Set Next Waypoint
 	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackBoardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cecle Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
 	return EBTNodeResult::Succeeded;
 }
