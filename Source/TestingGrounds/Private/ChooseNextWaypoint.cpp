2e8ba46634aee801ae0c64c82e6f3702c4918b61
AleksandraUrievna <kylie628659@gmail.com>
2020-06-02 21:13:43 +0300

    21. Composition over Inheritance
    We created PatrollingGuard by inheritance
    How about if we want a patrolling tank?
    Another option is to use an actor component
    This is an age-old debate
    Let’s get some real-world experience of the two
    Convert to a component model.

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -4,17 +4,23 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	// Get Patrol Points
-	auto AIController=OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointersCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard points"))
+		return EBTNodeResult::Failed;
+	}
 
 	//Set Next Waypoint
 	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
